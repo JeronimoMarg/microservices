8952d7d77495782ee9d030743aac568564954fbd
JeronimoMarg <jeronimomargitic@gmail.com>
2025-07-15 17:58:01 -0300

    primer commit

---
+++ GATEWAY/spring-gateway/src/main/java/com/example/spring_gateway/SpringGatewayApplication.java
@@ -0,0 +1,23 @@
+package com.example.spring_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGatewayApplication.class, args);
+	}
+	@Bean
+    @LoadBalanced
+    WebClient.Builder loadBalancedWebClientBuilder() {
+	   return WebClient.builder();
+    }
+
+}
